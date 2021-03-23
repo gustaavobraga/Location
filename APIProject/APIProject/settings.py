bd3c3af2093a4ff808efd69b1b0677f068663e54
Gustavo Braga <gustaa1320@gmail.com>
2021-03-23 10:41:22 -0300

    Update settings.py

--- APIProject/APIProject/settings.py
+++ APIProject/APIProject/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'i820#jx^air37gn7gb-_#fm5=r63g=kaia7c%qg32#ps=qv50_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
